a5b65a93ac3707737d0e0b974cca7449ed085b9a
omrajhalwa <omrajhalwa@gmail.com>
2024-09-22 01:04:06 +0530

    api calling in nextjs

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
 import Image from "next/image";
+import axios from "axios";
 
-export default function Home() {
+
+async function getUserDetails() {
+  await new Promise((r) => setTimeout(r,5000))
+  const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+	return response.data;
+}
+
+//async component only in server component
+export default async function Home() {
+
+  const userDetails = await getUserDetails();
   return (
-    <div>
+    <div className="">
       root route
+      {userDetails.email}
+      {userDetails.name}
     </div>
   );
 }
